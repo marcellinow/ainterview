de7a4dde6317048c4ee6252b6e8603e2f4277ec6
marcellinow <marcellino.gabriel12@gmail.com>
2024-12-20 20:38:56 +0700

    benerin path .css

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ainterview/",
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
   resolve: {
     extensions: [".js", ".jsx"],
   },
